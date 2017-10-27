334f88f78481cb9ae9176701c48377e5393af2e0
Jim Nelin <jim@jine.se>
2017-10-28 00:58:04 +0200

    Added old sourcecode from Makerspace.se
    Signed-off-by: Jim Nelin <jim@jine.se>

---
+++ application/views/footer.php
@@ -0,0 +1,11 @@
+	</div>
+	<br>
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+	<script src="/assets/js/bootstrap.min.js"></script>
+	<script src="/assets/js/jquery.metadata.js"></script>
+	<script src="/assets/js/jquery.tablesorter.min.js"></script>
+	<script src="/assets/js/jquery.tablesorter.pager.js"></script>
+	<script src="/assets/js/makerspace.js"></script>
+	</body>
+</html>
